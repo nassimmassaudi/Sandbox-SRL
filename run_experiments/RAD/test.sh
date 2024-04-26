4fdbaba1069b4d6703597c3c533144378632dcbd
Nassim M. <nassim.el.massaudi@umontreal.ca>
2024-04-26 05:15:16 +0000

    adding RAD v1

---
+++ run_experiments/RAD/test.sh
@@ -0,0 +1,9 @@
+xvfb-run -s "-screen 0 1400x900x24" python train_rad.py \
+    --domain_name cartpole \
+    --task_name swingup \
+    --encoder_type pixel --work_dir log \
+    --action_repeat 8 --num_eval_episodes 10 \
+    --pre_transform_image_size 100 --image_size 84 \
+    --agent rad_sac --frame_stack 3 --data_augs flip  \
+    --seed 23 --critic_lr 1e-3 --actor_lr 1e-3 --eval_freq 10000 --batch_size 128 --num_train_steps 200000 &
+ 
