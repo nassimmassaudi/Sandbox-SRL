cdfde59e1ab94db8fde95f2c26f8bf47314b9ca2
nassimmassaudi <nassim.el.massaudi@umontreal.ca>
2024-05-07 13:13:08 +0000

    update RAD run file

--- run_experiments/RAD/cheetah_rad.sh
+++ run_experiments/RAD/cheetah_rad.sh
@@ -6,11 +6,15 @@ xvfb-run -s "-screen 0 1400x900x24" python train_rad.py \
     --encoder_type pixel \
     --work_dir log/RAD \
     --action_repeat 4 \
-    --num_eval_episodes 10 \
+    --num_eval_episodes 20 \
     --agent rad_sac \
     --frame_stack 3 \
     --data_augs crop \
     --seed 1 \
+    --save_tb \
+    --save_video \
+    --save_wandb \
     --num_train_steps 100000 \
+    
 
 # Initially tested on 8 action repeat
