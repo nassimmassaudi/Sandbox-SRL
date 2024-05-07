00ef1a34028cd205342191b1d6ad8ac9486e8034
nassimmassaudi <nassim.el.massaudi@umontreal.ca>
2024-05-07 13:15:28 +0000

    fix RAD video

--- utils/utils_rad.py
+++ utils/utils_rad.py
@@ -365,7 +365,7 @@ class VideoRecorder(object):
     def save(self, file_name):
         if self.enabled:
             path = os.path.join(self.dir_name, file_name)
-            imageio.mimsave(path, self.frames, fps=self.fps)
+            imageio.mimsave(path, self.frames, fps=self.fps, format='GIF')
 
 
 from torch.utils.tensorboard import SummaryWriter
